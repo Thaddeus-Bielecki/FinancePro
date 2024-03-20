81b066afa63934b55e28066307de2bd4ef14c3da
Thaddeus-Bielecki <twb5147@truman.edu>
2024-03-20 13:51:36 -0500

    implemented the backend functionality for user queries (not admin)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,7 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id); //using the function from utils/generateToken.js
     
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name, 
       email: user.email,
@@ -54,93 +54,104 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/register
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => { 
-  // const { name, email, password } = req.body;
-  // const userExits = await User.findOne({ email });
-  // if (userExits) {
-  //   res.status(400);
-  //   throw new Error('User already exists.');
-  // }
-  // const user = await User.create({
-  //   name,
-  //   email,
-  //   password
-  // });
+  const { name, email, password } = req.body;
+  const userExits = await User.findOne({ email });
 
-  // if (user) {
-  //   generateToken(res, user._id);
-  //   res.status(201).json({
-  //     _id: user._id,
-  //     name: user.name, 
-  //     email: user.email,
-  //     isMember: user.isMember,
-  //     isAdmin: user.isAdmin,
-  //   });
-  // } else {
-  //   res.status(400);
-  //   throw new Error('Invalid user data. Please try again.');
-  // }
-  res.send('Register user');
+  if (userExits) {
+    res.status(400); // respond with 400 client error
+    throw new Error('User already exists.');
+  }
+
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name, 
+      email: user.email,
+      isMember: user.isMember,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data. Please try again.');
+  }
+  // res.send('Register user');
 });
 
 // @desc    Logout user & clear cookie
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => { 
-  // res.cookie('jwt', '', {
-  //   httpOnly: true,
-  //   expires: new Date(0),
-  // });
-  // res.status(200).json({ message: 'Logged out successfully.' });
-  res.send('Logout user');
+  // clear the cookie named 'jwt'
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: 'Logged out successfully.' });
+  // res.send('Logout user');
 });
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => { 
-  // const user = await User.findById(req.user._id);
-  // if (user) {
-  //   res.status(200).json({
-  //     _id: user._id,
-  //     name: user.name, 
-  //     email: user.email,
-  //     isMember: user.isMember,
-  //     isAdmin: user.isAdmin,
-  //   });
-  // } else {
-  //   res.status(404);
-  //   throw new Error('User not found.');
-  // }
-  res.send('Get user profile');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name, 
+      email: user.email,
+      isMember: user.isMember,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
+  // res.send('Get user profile');
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => { 
-  // const user = await User.findById(req.user._id);
-  // if (user) {
-  //   user.name = req.body.name || user.name;
-  //   user.email = req.body.email || user.email;
-  //   if (req.body.password) {
-  //     user.password = req.body.password;
-  //   }
+  const user = await User.findById(req.user._id);
 
-  //   const updatedUser = await user.save();
+  if (user) {
+    // dont have to update all fields -- only the ones that are sent
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
 
-  //   generateToken(res, updatedUser._id);
-  //   res.status(200).json({
-  //     _id: updatedUser._id,
-  //     name: updatedUser.name, 
-  //     email: updatedUser.email,
-  //     isMember: updatedUser.isMember,
-  //     isAdmin: updatedUser.isAdmin,
-  //   });
-  // } else {
-  //   res.status(404);
-  //   throw new Error('User not found.');
-  // }
-  res.send('Update user profile');
+    // if password is sent, update it since its hashed and needs to be re-hashed
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    // generateToken(res, updatedUser._id);
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name, 
+      email: updatedUser.email,
+      isMember: updatedUser.isMember,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
+  // res.send('Update user profile');
 });
 
 // @desc    Get all users
